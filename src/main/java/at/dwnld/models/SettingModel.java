cfc64bf895a68aecd623df3a2f7b4225a607e4f7
Adhishtanaka Kulasooriya <kulasoooriyaa@gmail.com>
2025-03-14 12:25:27 +0530

    improved SettingModel

--- src/main/java/at/dwnld/models/SettingModel.java
+++ src/main/java/at/dwnld/models/SettingModel.java
@@ -1,15 +1,28 @@
 package at.dwnld.models;
 
 public class SettingModel {
-    int default_path;
+    String default_path;
     int max_parallel;
     float default_speed;
     boolean is_default_application;
 
-    public SettingModel(int default_path, int max_parallel, float default_speed, boolean is_default_application) {
-        this.default_path = default_path;
+    public SettingModel(String default_path, int max_parallel, float default_speed, boolean is_default_application) {
+        this.default_path = (default_path != null) ? default_path : getDefaultDownloadDirectory();
         this.max_parallel = max_parallel;
         this.default_speed = default_speed;
         this.is_default_application = is_default_application;
+    }
+
+    public static String getDefaultDownloadDirectory() {
+        String userHome = System.getProperty("user.home");
+        if (userHome == null) return System.getProperty("java.io.tmpdir");
+
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win")) {
+            return userHome + "\\Downloads";
+        } else {
+            return userHome + "/Downloads";
+        }
+
     }
 }
